acff104e1d705baab04af7b58e6ef1023c1c12ea
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-29 00:53:02 -0500

    [INITIAL]

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">LOGO</header>
+      <main>LINKS</main>
+    </div>
+  );
+};
+
+export default Home;
